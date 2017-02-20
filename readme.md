9a36bf3b90e889f8764ca80b0b95ca9a9e5460ea
Kaidanov <2bhere4u@gmail.com>
2017-02-20 23:48:10 +0200

    Angular1 with bottle with mysql or just with json file serve full commit of the project

---
+++ readme.md
@@ -0,0 +1,9 @@
+**Angular 1 with bottle python serving data from MySQL**
+
+Can be used just serving data from json file.
+See comments in main.py
+
+Has shop_db.sql for mysql recreation on your pc.
+
+Screenshots:
+
--- shop_db.sql
+++ shop_db.sql
@@ -42,3 +42,34 @@ VALUES (1, "IPhone6" , "2daf04d9-e2b7-46ff-95c0-527273443f80", 10, "images/iphon
 INSERT INTO `description` (`id`, `products_id`, `fullDescription`, `IMAC`, `color`, `capacity`, `weight`, `dimentions`,  `instock`)
 VALUES (1, 1, "IPhone6 is a new apple phone", "f1b2dbe1-96b3-410c-abf8-8e5e913209b8", "'Silver','Space Gray'",
         "'16GB','64GB'", "Weight: 4.55 ounces (129 grams)", "138.1 mm * 67.0 mm", 1);
+
+INSERT INTO `products` (`id`, name, `serial`, `price`, `image`, `icon`, `description_id`)
+VALUES (2, "IPhone6" , "2daf04d9-e2b7-46ff-95c0-527273443f80", 10, "images/iphone6.jpg", "images/iphone6icon.jpg", 1);
+INSERT INTO `description` (`id`, `products_id`, `fullDescription`, `IMAC`, `color`, `capacity`, `weight`, `dimentions`,  `instock`)
+VALUES (2, 2, "IPhone6 is a new apple phone", "f1b2dbe1-96b3-410c-abf8-8e5e913209b8", "'Silver','Space Gray'",
+        "'16GB','64GB'", "Weight: 4.55 ounces (129 grams)", "138.1 mm * 67.0 mm", 1);
+
+INSERT INTO `products` (`id`, name, `serial`, `price`, `image`, `icon`, `description_id`)
+VALUES (3, "IPhone6" , "2daf04d9-e2b7-46ff-95c0-527273443f80", 10, "images/iphone6.jpg", "images/iphone6icon.jpg", 1);
+INSERT INTO `description` (`id`, `products_id`, `fullDescription`, `IMAC`, `color`, `capacity`, `weight`, `dimentions`,  `instock`)
+VALUES (3, 3, "IPhone6 is a new apple phone", "f1b2dbe1-96b3-410c-abf8-8e5e913209b8", "'Silver','Space Gray'",
+        "'16GB','64GB'", "Weight: 4.55 ounces (129 grams)", "138.1 mm * 67.0 mm", 1);
+
+INSERT INTO `products` (`id`, name, `serial`, `price`, `image`, `icon`, `description_id`)
+VALUES (4, "SamsungS6" , "52eb76c2-cc70-44a1-a215-f6f80b25b3c7", 11, "images/sm6.jpg", "images/sm6icon.jpg", 1);
+INSERT INTO `description` (`id`, `products_id`, `fullDescription`, `IMAC`, `color`, `capacity`, `weight`, `dimentions`,  `instock`)
+VALUES (4, 4, "SamsungGalaxyS6 is a new android phone", "ae282c02-aed5-4719-877c-689500d394c0", "'Silver','Space Gray'",
+        "'16GB','64GB'", "Weight: 4.55 ounces (129 grams)", "129.2 mm ", 1);
+
+
+INSERT INTO `products` (`id`, name, `serial`, `price`, `image`, `icon`, `description_id`)
+VALUES (5, "SamsungS6" , "52eb76c2-cc70-44a1-a215-f6f80b25b3c7", 11, "images/sm6.jpg", "images/sm6icon.jpg", 1);
+INSERT INTO `description` (`id`, `products_id`, `fullDescription`, `IMAC`, `color`, `capacity`, `weight`, `dimentions`,  `instock`)
+VALUES (5, 5, "SamsungGalaxyS6 is a new android phone", "ae282c02-aed5-4719-877c-689500d394c0", "'Silver','Space Gray'",
+        "'16GB','64GB'", "Weight: 4.55 ounces (129 grams)", "129.2 mm ", 1);
+
+INSERT INTO `products` (`id`, name, `serial`, `price`, `image`, `icon`, `description_id`)
+VALUES (6, "SamsungS6" , "52eb76c2-cc70-44a1-a215-f6f80b25b3c7", 11, "images/sm6.jpg", "images/sm6icon.jpg", 1);
+INSERT INTO `description` (`id`, `products_id`, `fullDescription`, `IMAC`, `color`, `capacity`, `weight`, `dimentions`,  `instock`)
+VALUES (6, 6, "SamsungGalaxyS6 is a new android phone", "ae282c02-aed5-4719-877c-689500d394c0", "'Silver','Space Gray'",
+        "'16GB','64GB'", "Weight: 4.55 ounces (129 grams)", "129.2 mm ", 1);
